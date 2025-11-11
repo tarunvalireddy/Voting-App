64c34e342716f7c23a27500445111b7a4d70b4db
Tarunvalireddy63 <145964752+tarunvalireddy@users.noreply.github.com>
2025-11-11 07:26:39 +0530

    Update server.js

--- result/server.js
+++ result/server.js
@@ -73,5 +73,5 @@ app.get('/', function (req, res) {
 
 server.listen(port, function () {
   var port = server.address().port;
-  console.log('App running on port ' + port);
+  console.log('App running on port ' + port); 
 });
